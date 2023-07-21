9973920d890f3d835b2c2b7373264884da375dda
MarceloHFilho <contato@omarceleza.com.br>
2023-07-21 20:38:38 -0300

    Melhorias

--- src/Pages/Camera.vue
+++ src/Pages/Camera.vue
@@ -1,17 +1,29 @@
 <template>
   <TakePic @snap="picture" />
-  <Gallery :images="images"/>
+  <Gallery :images="images" />
 </template>
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import TakePic from "../Components/TakePic.vue";
 import Gallery from "../Components/Gallery.vue";
 
-const pic = ref("");
+import db from "../database";
+
 const images = ref([]);
 
 const picture = (image) => {
   images.value.push(image);
+  db.files.add({
+    base64: image
+  })
 };
+
+onMounted(() => {
+  db.files.toArray().then((imgs) => {
+    imgs.forEach((img) => {
+      images.value.push(img.base64);
+    });
+  });
+})
 </script>
 <style scoped></style>
