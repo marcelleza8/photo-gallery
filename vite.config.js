c912b1baa996eac6eb9fb558d2ebea9edbe80f3f
MarceloHFilho <contato@omarceleza.com.br>
2023-07-21 17:53:41 -0300

    Add certificado para funcionar com TLS no localhost.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import * as fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    cors: {
+      origin: "*",
+    },
+    https: {
+      key: fs.readFileSync("./.docker/nginx/certs/localhost-key.pem"),
+      cert: fs.readFileSync("./.docker/nginx/certs/localhost.pem"),
+    },
+  },
+});
